bd86e7c8acf43692f7b42c5415fa0b3953007dcd
Choi A Ram <df0404@hanmail.net>
2021-08-11 10:27:18 +0900

    [update] 최아람

---
+++ ensemble_studio/src/main/java/com/kh/ensemble/auth/SnsValue.java
@@ -0,0 +1,72 @@
+package com.kh.ensemble.auth;
+
+import org.apache.commons.lang3.StringUtils;
+
+import com.github.scribejava.core.builder.api.DefaultApi20;
+
+public class SnsValue implements SnsUrls {
+	private String service;
+	private String clientId;
+	private String clientSecret;
+	private String redirectUrl;
+	private DefaultApi20 api20Instance;
+	private String profileUrl;
+
+	public SnsValue(String service, String clientId, String clientSecret, String redirectUrl) {
+		this.service = service;
+		this.clientId = clientId;
+		this.clientSecret = clientSecret;
+		this.redirectUrl = redirectUrl;
+		this.api20Instance = NaverAPI20.instance();
+		this.profileUrl = NAVER_PROFILE_URL;
+	}
+
+	public String getProfileUrl() {
+		return profileUrl;
+	}
+
+	public void setProfileUrl(String profileUrl) {
+		this.profileUrl = profileUrl;
+	}
+
+	public String getService() {
+		return service;
+	}
+
+	public void setService(String service) {
+		this.service = service;
+	}
+
+	public String getClientId() {
+		return clientId;
+	}
+
+	public void setClientId(String clientId) {
+		this.clientId = clientId;
+	}
+
+	public String getClientSecret() {
+		return clientSecret;
+	}
+
+	public void setClientSecret(String clientSecret) {
+		this.clientSecret = clientSecret;
+	}
+
+	public String getRedirectUrl() {
+		return redirectUrl;
+	}
+
+	public void setRedirectUrl(String redirectUrl) {
+		this.redirectUrl = redirectUrl;
+	}
+
+	public DefaultApi20 getApi20Instance() {
+		return api20Instance;
+	}
+
+	public void setApi20Instance(DefaultApi20 api20Instance) {
+		this.api20Instance = api20Instance;
+	}
+
+}
